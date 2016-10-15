22995bef84b0a0547458829cdc99bb5f217d293a
sgnl <rayrfarias@gmail.com>
2016-10-14 21:01:38 -1000

    found network ip :fireworks:

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const App = require('express')()
 const Server = require('http').Server(App)
 const IO = require('socket.io')(Server)
+const PORT = 8081;
 
 const BodyParser = require('body-parser')
 
 const Secret = require('./lib/secret')()
 const SockPuppet = require('./lib/puppet')()
+
+const Interfaces = require('os').networkInterfaces()
+
+const NetworkIP = Object.keys(Interfaces)
+  .map(key => {
+    return Interfaces[key]
+      .filter(connection => {
+        return connection.family === 'IPv4' && !connection.internal
+      })[0]
+  })
+  .filter(x => x)[0].address
 
 App.use(BodyParser.urlencoded({extended: true}))
 
@@ -33,18 +45,14 @@ App.post('/bouncer', (req, res) => {
         username: req.body.username,
         guess: req.body.guess
       })
-
     } else {
       resBody.success = true
       resBody.message = 'GRATS YOU GET SAUCE!!!! PLAYING VIDEO ~~~ secret has been increased'
 
       SockPuppet.emit('video_change', {video_id: req.body.video_id})
-
     }
-
     return res.json(resBody)
   })
 })
 
-Server.listen(8081)
-
+Server.listen(PORT, _ => process.stdout.write(`tube-sock-puppet server running @ http://${NetworkIP}:${PORT}\r\n`))
