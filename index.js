9a0c9caf249bd0835f19423963935d2687eab9c4
sgnl <rayrfarias@gmail.com>
2016-10-14 16:04:19 -1000

    successful post request changes video, yay!

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const App = require('express')()
 const Server = require('http').Server(App)
 const IO = require('socket.io')(Server)
+
 const BodyParser = require('body-parser')
+
 const Secret = require('./lib/secret')()
+const SockPuppet = require('./lib/puppet')()
 
 App.use(BodyParser.urlencoded({extended: true}))
+
+IO.on('connection', (socket) => {
+  SockPuppet.init(socket)
+  socket.emit('news', { hello: 'world' })
+
+  socket.on('client', function (data) {
+    console.log('client connected')
+  })
+
+})
 
 App.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -20,16 +33,10 @@ App.post('/bouncer', (req, res) => {
     } else {
       resBody.success = true
       resBody.message = 'GRATS YOU GET SAUCE!!!! PLAYING VIDEO ~~~ secret has been increased'
+      SockPuppet.emit('video_change', {video_id: req.body.video_id})
     }
 
     return res.json(resBody)
-  })
-})
-
-IO.on('connection', (socket) => {
-  socket.emit('news', { hello: 'world' })
-  socket.on('my other event', function (data) {
-    console.log(data)
   })
 })
 
