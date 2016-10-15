5b75d4310afa6aff9cc37278537b4f31cfebf217
sgnl <rayrfarias@gmail.com>
2016-10-14 14:47:00 -1000

    adds body-parser; stubs post route

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const App = require('express')()
 const Server = require('http').Server(App)
 const IO = require('socket.io')(Server)
+const BodyParser = require('body-parser')
+
+App.use(BodyParser.urlencoded({extended: true}))
 
 App.get('/', (req, res) => {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html')
 })
 
 App.post('/bouncer', (req, res) => {
-  console.log('req.body: ', req.body);
+  res.json(req.body)
 })
 
 IO.on('connection', (socket) => {
